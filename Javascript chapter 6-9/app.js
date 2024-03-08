e9bb458721b80d78d4617cac8342895eb81243c5
Muhammad Muddassir <155112096+qmmuddi@users.noreply.github.com>
2024-03-08 07:18:20 +0500

    new

---
+++ Javascript chapter 6-9/app.js
@@ -0,0 +1,14 @@
+var user_input =prompt("which one is your subject?")
+if ("user_input===mathematics"){
+    console.log("hello sir")
+}
+else if("user_input===physics"){
+    console.log("amazing subject")
+}
+else if("user_input_===chemistry"){
+    console.log("best subject to learn")
+}
+else{
+    console.log("subject not valid")
+}
+var user_input1 =prompt("")
